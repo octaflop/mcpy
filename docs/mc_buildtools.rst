3dad7241f63955cdb60f0871b591a9e8685edc83
Faris Chebib <faris@zaneprep.com>
2016-02-07 16:23:37 -0700

    more biuld scripts

--- docs/mc_buildtools.rst
+++ docs/mc_buildtools.rst
@@ -11,3 +11,5 @@ I wrote a python invoke task to handle this:
     inv get_build_tools
 
 The build tools will install in the ``mcbuild`` directory. This directory has been excluded from the git repo.
+
+The build tools will download a bunch of repos including ``CraftBukkit``.
--- tasks.py
+++ tasks.py
@@ -4,7 +4,13 @@ from __future__ import unicode_literals
 from invoke import ctask as task
 import webbrowser
 
+import git
 import os
+
+# Get working directory
+REL = os.path.dirname(os.path.realpath(__file__))
+BUILD_PATH = os.path.join(REL, "mcbuild")
+
 
 @task
 def docs(ctx):
@@ -17,3 +23,23 @@ def get_build_tools(ctx):
     ctx.run("mkdir mcbuild; wget -O mcbuild/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
     os.chdir("mcbuild")
     ctx.run("java -jar BuildTools.jar")
+    os.chdir("/tmp")
+    ctx.run("wget -O lpm.zip https://sourceforge.net/projects/python-with-minecraft-windows/files/latest/download")
+    ctx.run("unzip lpm.zip -d lpm")
+    ctx.run("mv lpm/Minecraft\ Tools/server/ {}".format(BUILD_PATH))
+
+
+@task
+def start_server(ctx):
+    os.chdir("mcbuild")
+    ctx.run("java -jar craftbukkit-1.8.8.jar")
+
+
+@task
+def get_plugins(ctx):
+    """
+    Clones plugins
+    """
+    os.chdir("..")
+    git.Git().clone("https://github.com/py3minepi/py3minepi")
+    os.chdir("py3minepi")
