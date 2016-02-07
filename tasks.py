462c2bd6978a940b9b0029524247a109ec47cf93
Faris Chebib <faris@zaneprep.com>
2016-02-07 15:55:19 -0700

    updated buildtools task

--- tasks.py
+++ tasks.py
@@ -9,3 +9,9 @@ import webbrowser
 def docs(ctx):
     ctx.run("sphinx-autobuild docs docs/_build/html/ -p 8001")
     webbrowser.open("http://127.0.0.1:8001")
+
+
+@task
+def get_build_tools(ctx):
+    ctx.run("wget -O mcbuild/BuildTools https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+    ctx.run("cd mcbuild && java -jar BuildTools.jar")
