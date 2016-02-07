439b135ffec54a17af70f9547485e979682bf1c2
Faris Chebib <faris@zaneprep.com>
2016-02-07 16:29:17 -0700

    set to mac source

--- tasks.py
+++ tasks.py
@@ -24,7 +24,7 @@ def get_build_tools(ctx):
     os.chdir("mcbuild")
     ctx.run("java -jar BuildTools.jar")
     os.chdir("/tmp")
-    ctx.run("wget -O lpm.zip https://sourceforge.net/projects/python-with-minecraft-windows/files/latest/download")
+    ctx.run("wget -O lpm.zip https://sourceforge.net/projects/python-with-minecraft-mac/files/latest/download")
     ctx.run("unzip lpm.zip -d lpm")
     ctx.run("rsync -rltvh --progress lpm/Minecraft\ Tools/server/ {}/".format(BUILD_PATH))
 
