bdc3264f78d30e9add623221638a144e65cce29b
Faris Chebib <faris@zaneprep.com>
2016-02-08 12:30:48 -0700

    fixed tasks

--- tasks.py
+++ tasks.py
@@ -14,40 +14,43 @@ BUILD_PATH = os.path.join(REL, "mcbuild")
 
 @task
 def docs(ctx):
+    """
+    Compiles and starts up a docs server
+    """
+    os.chdir("docs")
+    ctx.run("make html")
+    os.chdir(REL)
     ctx.run("sphinx-autobuild docs docs/_build/html/ -p 8001")
     webbrowser.open("http://127.0.0.1:8001")
 
 
 @task
 def get_build_tools(ctx):
+    """
+    Main installation of build tools & Minecraft Python server
+    """
     ctx.run("mkdir mcbuild; wget -O mcbuild/BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
     os.chdir("mcbuild")
     ctx.run("java -jar BuildTools.jar")
     os.chdir("/tmp")
     ctx.run("wget -O lpm.zip https://sourceforge.net/projects/python-with-minecraft-mac/files/latest/download")
     ctx.run("unzip lpm.zip -d lpm")
-
-@task
-def copy_world(ctx):
     ctx.run("rsync -rltvh --progress /tmp/lpm/MinecraftTools/server/ {}/".format(BUILD_PATH))
 
 @task
 def clean_build_tools(ctx):
+    """
+    Removes the mcbuild folder
+    """
     os.chdir(REL)
     ctx.run("rm -rvf mcbuild/")
 
 
 @task
 def start_server(ctx):
+    """
+    Starts the Minecraft Python server
+    """
     os.chdir("mcbuild")
     ctx.run("java -jar craftbukkit-1.8.8.jar")
 
-
-@task
-def get_plugins(ctx):
-    """
-    Clones plugins
-    """
-    os.chdir("..")
-    git.Git().clone("https://github.com/py3minepi/py3minepi")
-    os.chdir("py3minepi")
