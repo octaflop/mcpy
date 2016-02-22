a9038263ae3cd34e67961dd03710257ee1e0007b
Faris Chebib <faris@zaneprep.com>
2016-02-21 17:39:21 -0700

    random tower

--- examples/random_tower.py
+++ examples/random_tower.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import random
 import mcpi.block as block
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
@@ -11,4 +12,4 @@ x, y, z = position.x, position.y, position.z
 mc.player.setTilePos(x, y, z)
 
 for b in range(-5, 20):
-    mc.setBlock(x, y+b, z, 103)
+    mc.setBlock(x, y+b, z, 103+random.randint(0, 100))
