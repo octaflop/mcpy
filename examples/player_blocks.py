d5e68ed7edbad0441f18e952c3beee4ec4e87031
Faris Chebib <faris@zaneprep.com>
2016-02-27 17:11:10 -0700

    added mandelbrot example

--- examples/player_blocks.py
+++ examples/player_blocks.py
@@ -2,13 +2,12 @@
 from __future__ import unicode_literals
 
 import time
-import mcpi.block as block
 
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
 while True:
     x, y, z = mc.player.getTilePos()
-    b = mc.getBlock(x, y-1, z)
+    b = mc.getBlock(x, y - 1, z)
     mc.postToChat("Standing on: {}".format(b))
     time.sleep(2)
