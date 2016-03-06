047cafb228c1e22b0f56656c68bc4aa27c604fe7
David Ernstrom <david@ernstrom.net>
2016-03-06 11:03:48 -0700

    PEP8 and optimizing of rainbow example.

--- examples/rainbow.py
+++ examples/rainbow.py
@@ -1,37 +1,37 @@
 # -*- coding: utf-8 -*-
+"""A simple rainbow script
+Inspired by:
+http://www.minecraftforum.net/forums/other-platforms/minecraft-pi-edition/1959851-my-first-script-for-minecraft-pi-api-a-rainbow
+"""
 from __future__ import unicode_literals
-
 from math import sin, pi
 
-import mcpi.block as block
+from mcpi import block
 from mcpi.minecraft import Minecraft
+
+
+RED = 14
+ORANGE = 1
+YELLOW = 4
+LIME = 5
+LIGHT_BLUE = 3
+BLUE = 11
+PURPLE = 10
+COLORS = (RED, ORANGE, YELLOW, LIME, LIGHT_BLUE, BLUE, PURPLE)
+RAINBOW_HEIGHT = 60
+RAINBOW_WIDTH = 128
+RAINBOW_HALF = RAINBOW_WIDTH / 2
+
 mc = Minecraft.create()
-
-
-"""
-A simple rainbow script
-
-Inspired by: http://www.minecraftforum.net/forums/other-platforms/minecraft-pi-edition/1959851-my-first-script-for-minecraft-pi-api-a-rainbow
-"""
-
-colors = [14, 1, 4, 5, 3, 11, 10]
-height = 60
-
 # unpack player position
-
 position = mc.player.getTilePos()
 px, py, pz = position.x, position.y, position.z
 
-# clears blocks in path
-mc.setBlocks(
-    -64+px, py, pz, 64+px, height + len(colors)+py, pz, 0)
-for x in range(0, 128):
-    for ci in range(0, len(colors)):
-        y = sin((x / 128.0) * pi) * height + ci
-        x = (x - 64) 
+for curve_x in range(0, RAINBOW_WIDTH):
+    curve = sin(pi * float(curve_x) / float(RAINBOW_WIDTH)) * RAINBOW_HEIGHT
+    for color in COLORS:
+        map_pos_y = int(curve) - COLORS.index(color)
         mc.setBlock(
-            x,
-            y,
-            0,
+            curve_x - RAINBOW_HALF + px, map_pos_y + py, pz,
             block.WOOL.id,
-            colors[len(colors) - 1 - ci])
+            color)
